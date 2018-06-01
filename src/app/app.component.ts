ecc6012dbb2ea1c157953516da0a6a571e839a9f
Timur Daudov <timur.daudov@namecheap.com>
2018-06-01 20:05:57 +0300

    Add tour of heroes app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
 }
